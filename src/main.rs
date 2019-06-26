34882825ecf06880199eb856d5aca4224a2d8781
Alex Afshar <contact@alexafshar.com>
2019-06-25 20:38:56 -0400

    Added one long function to get input.

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,27 @@
 use std::io::{stdin,stdout,Write};
 
 fn main() {
-    let mut name=String::new();
-
-    print!("Please enter your name: ");
-
-    // Flust stdout
-    let _=stdout().flush();
-
-    stdin().read_line(&mut name).expect("Error reading input");
-
-    if let Some('\n')=name.chars().next_back() {
-        name.pop();
-    }
-
-    if let Some('\r')=name.chars().next_back() {
-        name.pop();
-    }
+    let name = get_input("Please enter your name: ");
 
     println!("Hello, {}", name);
+}
+
+fn get_input(msg: &'static str) -> String {
+    let mut input = String::new();
+
+    print!("{}", msg); // Print message.
+
+    let _=stdout().flush(); // Flust stdout.
+
+    stdin().read_line(&mut input).expect("Error reading input");
+    // Remove newline.
+    if let Some('\n')=input.chars().next_back() {
+        input.pop();
+    }
+    // Remove carriage return.
+    if let Some('\r')=input.chars().next_back() {
+        input.pop();
+    }
+
+    input // Return cleaned input.
 }
