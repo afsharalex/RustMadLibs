df4f432df0c8d29109bbf644c3ff01312be48bb1
Alex Afshar <contact@alexafshar.com>
2019-06-25 21:39:15 -0400

    Cleaned MadLib methods
    Had mucked up the `self` args in the MadLibs. Self is sugar for
    current object, self will take ownership, &self would be the reference
    to current object.

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ struct MadLib {
 
 impl MadLib {
     fn new(name: String, subject: String,
-           verb: String, object: String) -> MadLib {
+           verb: String, object: String) -> Self {
 
         MadLib {
             name: name, subject: subject,
@@ -17,7 +17,7 @@ impl MadLib {
         }
     }
 
-    fn create() -> MadLib {
+    fn create() -> Self {
         let name = get_input("Please enter your name: ");
         let subject = get_input("Please enter a subject: ");
         let verb = get_input("Please enter a verb (past tense): ");
@@ -26,7 +26,7 @@ impl MadLib {
         MadLib::new(name, subject, verb, object)
     }
 
-    fn print(self) {
+    fn print(&self) {
         println!("{name} {verb} the {subject} with their {object}",
                  name=self.name,
                  verb=self.verb,
@@ -44,7 +44,7 @@ fn get_input(msg: &'static str) -> String {
 
     print!("{}", msg); // Print message.
 
-    let _=stdout().flush(); // Flust stdout.
+    let _=stdout().flush(); // Flush stdout.
 
     stdin().read_line(&mut input).expect("Error reading input");
 
